505295c84452145983ba82297b50ed35ee839138
pavel912 <gta4thebest@mail.ru>
2023-04-10 20:24:06 +0300

    add user entity

---
+++ app/src/main/java/hexlet/code/domain/User.java
@@ -0,0 +1,34 @@
+package hexlet.code.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @CreatedDate
+    private Instant createdAt;
+}
